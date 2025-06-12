e14cd4ff9bfec64a3308344ddc477ac88436ce1e
veerrajukakarla434 <kakarlaveerraju2010@gmail.com>
2025-06-12 18:28:53 +0530

    Veer:  Code re factored, and removed unnecessary annotations

--- 2025-SpringBoot-MySql-ReactJs-CRUD-Example/src/main/java/com/vkakarla/ems/Application.java
+++ 2025-SpringBoot-MySql-ReactJs-CRUD-Example/src/main/java/com/vkakarla/ems/Application.java
@@ -2,26 +2,7 @@ package com.vkakarla.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-
-@OpenAPIDefinition(info = @Info(title = "Employee Management Project", 
-version = "0.0.1-SNAPSHOT", description = "Employee Service Operations", 
-termsOfService = "vkakarla java springboot service", 
-contact = @Contact(name = "vkakarl", email = "vkakarla1985@gmail.com"), 
-license = @License(name = "vkakarla", url = "http://youtube.com/vkakarla")))
 @SpringBootApplication
-@EnableConfigurationProperties
-@EnableJpaRepositories(basePackages = { "com.vkakarla.ems.repo" })
-@ComponentScan(value = "com.vkakarla.ems.*")
-@EntityScan(basePackages = { "com.vkakarla.ems.entities" })
 public class Application {
 	
 	public static void main(String[] args) {	
